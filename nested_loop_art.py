fb5bc14937a780534f48b098faa8fa71456b8297
Ugochukwu <emdeefx@gmail.com>
2024-06-27 00:34:33 +0100

    Added comments in  file nested_loop_art.py

--- nested_loop_art.py
+++ nested_loop_art.py
@@ -1,12 +1,12 @@
 rows = 5
 column = 1
 
-while rows > 0:
-    while column < 10:
-        for space in range(1, rows):
+while rows > 0:                              #loop to make 5 rows (0-4)
+    while column < 10:                       #loop to increase the number of stars per column (starting from 1)
+        for space in range(1, rows):         #for space to match each row
             print(end=' ')
-        for i in range(1, column + 1):
+        for i in range(1, column + 1):       #to iterate through the value of column
             print('*',end='')
-        print()
+        print()                              #prints a new space after each iteration
         column += 2
         rows -= 1
