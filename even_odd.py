57fb266bdd02e1679e32e7ea07fdd1b11c18ddac
Ugochukwu <emdeefx@gmail.com>
2024-07-01 10:11:27 +0100

    Created (3)files greeting_func, return_statement, even_odd. This was for the purpose of practicing defining a function

---
+++ even_odd.py
@@ -0,0 +1,8 @@
+def even_odd(number):
+    if number%2 == 0:
+        print("The number is even")
+    else:
+        print("The number is odd")
+
+input = int(input("Enter a number: "))
+even_odd(input)
---
+++ return_statement.py
@@ -0,0 +1,7 @@
+def rectangle_area(length, width):
+    area = length * width
+    return area
+
+shup = rectangle_area(10, 5)
+
+print(shup)
