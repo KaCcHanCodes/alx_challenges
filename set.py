3e53d25d7b4c6e7f19a62c559a009169ea0a02c0
Ugochukwu <emdeefx@gmail.com>
2024-07-02 16:36:03 +0100

    Added comments to file set.py

--- set.py
+++ set.py
@@ -1,12 +1,16 @@
 import random
 
+#generate random numbers
 numbers = random.randint(1, 10)
 count = 5
 list = []
 
+#generate a list of random numbers 5 times
 while count > 0:
     for i in range(1, numbers):
+        #store each value of i in list
         list.append(i)
     count -= 1
+#remove duplicate numbers
 x = set(list)
 print(x)
